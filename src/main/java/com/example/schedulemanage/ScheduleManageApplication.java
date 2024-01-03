9d04c5c351b26ca49e1323dc0776aed2c8d5e482
gunhee <rjsgml1677@naver.com>
2024-01-03 11:32:30 +0900

    left-sidebar 관련작업반영

---
+++ src/main/java/com/example/schedulemanage/ScheduleManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulemanage/ScheduleManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
